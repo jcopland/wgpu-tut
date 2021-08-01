5ad808816d8a77e71589135a3980953701902387
Jack Copland <jack.copland@rea-group.com>
2021-08-01 21:01:17 +1000

    Added resizing functionality to the window

--- src/main.rs
+++ src/main.rs
@@ -60,7 +60,10 @@ impl State {
     }
 
     fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
-        todo!()
+        self.size = new_size;
+        self.sc_desc.width = new_size.width;
+        self.sc_desc.height = new_size.height;
+        self.swap_chain = self.device.create_swap_chain(&self.surface, &self.sc_desc);
     }
 
     fn input(&mut self, event: &WindowEvent) -> bool {
